ed372cef3bc5dd351d20083593382df9b92b73db
RahulPerumandla <rahul.perumandla@reancloud.com>
2017-07-19 14:35:23 -0400

    Update default.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -22,18 +22,9 @@ package "php-mysql" do
 	action :install
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/mysql-community-release-el7-5.noarch.rpm" do
-      source 'http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm'
-        action :create
-end
-
-rpm_package 'mysql-community-release' do
-      source "#{Chef::Config[:file_cache_path]}/mysql-community-release-el7-5.noarch.rpm"
-        action :install
-end
 
 
-package "mariadb" do
+package "mysql" do
 	action :install
 end
 
